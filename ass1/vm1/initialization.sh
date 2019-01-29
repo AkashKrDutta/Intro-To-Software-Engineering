293cf5f26761acf12de47ade5b70d7674473b979
Akash Kumar Dutta <akdboltzmann@gmail.com>
2019-01-30 03:53:49 +0530

    Add README

--- ass1/vm1/initialization.sh
+++ ass1/vm1/initialization.sh
@@ -12,3 +12,5 @@ apt-get install git
 #extract files for vm1 to /var/www/html
 git clone https://github.com/AkashKrDutta/Intro-To-Software-Engineering
 cp ./Intro-To-Software-Engineering/ass1/vm1/* /var/www/html/
+
+
