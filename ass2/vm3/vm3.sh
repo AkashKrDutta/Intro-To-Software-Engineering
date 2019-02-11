1549d97b4f61b67a56d63bb3f74b5f6f40f7a548
Akash Kumar Dutta <akdboltzmann@gmail.com>
2019-02-11 18:45:28 +0530

    Basic thing works, html left

--- ass2/vm3/vm3.sh
+++ ass2/vm3/vm3.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#Change this ip to that of vm1 
+#Change this ip and user to that of vm1 
 ip1=192.168.0.113
+user1=akash
 
 #Change this ip to that of vm2
 ip2=192.168.0.114
+user2=akash2
 
 if [[ "$#" -ne 1 ]]; then
     echo "Usage: ./vm3.sh <ping frequency in secs>"
@@ -20,7 +22,8 @@ do
             echo "Host vm1 Unreachable!"
     else
 	    echo "vm1 status:"
-            systemctl --host kaushal@$ip1 is-active vm1.service
+
+            systemctl --host $user1@$ip1 is-active vm1.service
             
     fi
 
@@ -30,8 +33,8 @@ do
     then
             echo "Host vm2 Unreachable!"
     else
-	    echo "vm1 status:"
-            systemctl --host kaushal@$ip1 is-active vm1.service
+	    echo "vm2 status:"
+            systemctl --host $user2@$ip2 is-active vm2.service
             
     fi
  
